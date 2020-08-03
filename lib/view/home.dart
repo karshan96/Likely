a810871c967fa22be9f0d12b219b908db6f20669
Rasa Karshanguna <karshanguna@gmail.com>
2020-08-03 16:11:34 +0530

    home page created

---
+++ lib/view/home.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:Likely/view/login.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset('assets/images/title.jpg',
+                fit: BoxFit.cover, height: 32),
+            Text("Likely", style: TextStyle(color: Colors.green))
+          ],
+        ),
+        centerTitle: true,
+        elevation: 0.0,
+      ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          FlatButton(
+              child: Text('Login'),
+              onPressed: () {
+                LogIn();
+              },
+              textColor: Colors.black),
+          FlatButton(
+              child: Text('Register'),
+              onPressed: () {},
+              textColor: Colors.black),
+          FlatButton(
+              child: Text('Post Your Ad'),
+              onPressed: () {},
+              color: Colors.orange,
+              textColor: Colors.white)
+        ],
+      ),
+    );
+  }
+}
